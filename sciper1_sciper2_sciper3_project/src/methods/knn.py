7ef094c528547d38247e8a459a8fef881c0fc0d1
gkerjan <gersende.kerjan@epfl.ch>
2025-04-04 17:08:28 +0200

    implemented knn and logistic_regression

--- sciper1_sciper2_sciper3_project/src/methods/knn.py
+++ sciper1_sciper2_sciper3_project/src/methods/knn.py
@@ -27,12 +27,10 @@ class KNN(object):
                 pred_labels (np.array): labels of shape (N,)
         """
 
-        ##
-        ###
-        #### YOUR CODE HERE!
-        ###
-        ##
-        return pred_labels
+        self.training_data = training_data
+        self.training_labels = training_labels
+
+        return training_labels
 
     def predict(self, test_data):
         """
@@ -43,9 +41,82 @@ class KNN(object):
             Returns:
                 test_labels (np.array): labels of shape (N,)
         """
-        ##
-        ###
-        #### YOUR CODE HERE!
-        ###
-        ##
+        
+        test_labels = self.kNN(test_data, self.training_data, self.training_labels, self.k)
+
         return test_labels
+    
+    def euclidean_dist(example, training_examples):
+        """Compute the Euclidean distance between a single example
+        vector and all training_examples.
+
+        Inputs:
+            example: shape (D,)
+            training_examples: shape (NxD) 
+        Outputs:
+            euclidean distances: shape (N,)
+        """
+        return np.sum((training_examples - example)**2, axis=1)**(1/2)
+    
+    def find_k_nearest_neighbors(k, distances):
+        """ Find the indices of the k smallest distances from a list of distances.
+            Tip: use np.argsort()
+
+        Inputs:
+            k: integer
+            distances: shape (N,) 
+        Outputs:
+            indices of the k nearest neighbors: shape (k,)
+        """
+        
+        indices = np.argsort(distances)[:k]
+        return indices
+    
+    def predict_label(neighbor_labels):
+        """Return the most frequent label in the neighbors'.
+
+        Inputs:
+            neighbor_labels: shape (N,) 
+        Outputs:
+            most frequent label
+        """
+        
+        return np.argmax(np.bincount(neighbor_labels))
+    
+    def kNN_one_example(self, unlabeled_example, training_features, training_labels, k):
+        """Returns the label of a single unlabelled example.
+
+        Inputs:
+            unlabeled_example: shape (D,) 
+            training_features: shape (NxD)
+            training_labels: shape (N,) 
+            k: integer
+        Outputs:
+            predicted label
+        """
+        # Compute distances
+        distances = self.euclidean_dist(unlabeled_example, training_features)
+        
+        # Find neighbors
+        nn_indices = self.find_k_nearest_neighbors(k, distances)
+        
+        # Get neighbors' labels
+        neighbor_labels = training_labels[nn_indices]
+        
+        # Pick the most common
+        best_label = self.predict_label(neighbor_labels)
+        
+        return best_label
+    
+    def kNN(self, unlabeled, training_features, training_labels, k):
+        """Return the labels vector for all unlabeled datapoints.
+
+        Inputs:
+            unlabeled: shape (MxD) 
+            training_features: shape (NxD)
+            training_labels: shape (N,) 
+            k: integer
+        Outputs:
+            predicted labels: shape (M,)
+        """
+        return np.apply_along_axis(self.kNN_one_example, 1, self, unlabeled, training_features, training_labels, k)
--- sciper1_sciper2_sciper3_project/src/methods/logistic_regression.py
+++ sciper1_sciper2_sciper3_project/src/methods/logistic_regression.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from ..utils import get_n_classes, label_to_onehot, onehot_to_label
+from ..utils import get_n_classes, label_to_onehot, onehot_to_label, accuracy_fn
 
 
 class LogisticRegression(object):
@@ -31,11 +31,10 @@ class LogisticRegression(object):
         Returns:
             pred_labels (array): target of shape (N,)
         """
-        ##
-        ###
-        #### WRITE YOUR CODE HERE!
-        ###
-        ##
+
+        self.W = self.logistic_regression_train_multi(self, training_data, label_to_onehot(training_labels, get_n_classes(training_labels)))
+        pred_labels = onehot_to_label(self.logistic_regression_predict_multi(self, training_data, self.W))
+
         return pred_labels
 
     def predict(self, test_data):
@@ -47,9 +46,92 @@ class LogisticRegression(object):
         Returns:
             pred_labels (array): labels of shape (N,)
         """
-        ##
-        ###
-        #### WRITE YOUR CODE HERE!
-        ###
-        ##
+
+        pred_labels = onehot_to_label(self.logistic_regression_predict_multi(self, test_data, self.W))
+
         return pred_labels
+
+    def f_softmax(data, W):
+        """
+        Softmax function for multi-class logistic regression.
+        
+        Args:
+            data (array): Input data of shape (N, D)
+            W (array): Weights of shape (D, C) where C is the number of classes
+        Returns:
+            array of shape (N, C): Probability array where each value is in the
+                range [0, 1] and each row sums to 1.
+                The row i corresponds to the prediction of the ith data sample, and 
+                the column j to the jth class. So element [i, j] is P(y_i=k | x_i, W)
+        """
+        weights = np.apply_along_axis(np.exp, 0, data @ W)
+        sum_under = np.sum(weights, axis = 1)
+        return (weights.T/sum_under).T
+    
+    def loss_logistic_multi(self, data, labels, w):
+        """ 
+        Loss function for multi class logistic regression, i.e., multi-class entropy.
+        
+        Args:
+            data (array): Input data of shape (N, D)
+            labels (array): Labels of shape  (N, C)  (in one-hot representation)
+            w (array): Weights of shape (D, C)
+        Returns:
+            float: Loss value 
+        """
+        return -np.sum(np.sum(labels * np.log(self.f_softmax(data, w))))
+    
+    def gradient_logistic_multi(self, data, labels, W):
+        """
+        Compute the gradient of the entropy for multi-class logistic regression.
+        
+        Args:
+            data (array): Input data of shape (N, D)
+            labels (array): Labels of shape  (N, C)  (in one-hot representation)
+            W (array): Weights of shape (D, C)
+        Returns:
+            grad (np.array): Gradients of shape (D, C)
+        """
+        return data.T @ (self.f_softmax(data, W) - labels)
+    
+    def logistic_regression_predict_multi(self, data, W):
+        """
+        Prediction the label of data for multi-class logistic regression.
+        
+        Args:
+            data (array): Dataset of shape (N, D).
+            W (array): Weights of multi-class logistic regression model of shape (D, C)
+        Returns:
+            array of shape (N,): Label predictions of data.
+        """
+        return np.apply_along_axis(np.argmax, 1, self.f_softmax(data, W))
+
+    def logistic_regression_train_multi(self, data, labels):
+        """
+        Training function for multi class logistic regression.
+        
+        Args:
+            data (array): Dataset of shape (N, D).
+            labels (array): Labels of shape (N, C)
+            max_iters (int): Maximum number of iterations. Default: 10
+            lr (int): The learning rate of  the gradient step. Default: 0.001
+            print_period (int): Number of iterations to print current loss. 
+                If 0, never printed.
+            plot_period (int): Number of iterations to plot current predictions.
+                If 0, never plotted.
+        Returns:
+            weights (array): weights of the logistic regression model, of shape(D, C)
+        """
+        D = data.shape[1]  # number of features
+        C = labels.shape[1]  # number of classes
+        # Random initialization of the weights
+        weights = np.random.normal(0, 0.1, (D, C))
+        for it in range(self.max_iters):
+            gradient = self.gradient_logistic_multi(self, data, labels, weights)
+            weights = weights - (self.lr * gradient)
+
+            predictions = self.logistic_regression_predict_multi(self, data, weights)
+            if accuracy_fn(predictions, onehot_to_label(labels)) == 100:
+                break
+            
+        return weights
